2b1055aebfe42776178f595664d7372de86cf096
Stan S <stan.shm@gmail.com>
2022-12-15 20:11:26 -0600

    index route added

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use('/checkout', checkoutRouter);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('/build'));
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
