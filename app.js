058f1abf7933710208c7beb5025ee7cd5b96dd96
Stas <stan.shm@gmail.com>
2024-10-07 20:58:14 -0500

    attempt to deploy to azure 2

--- app.js
+++ app.js
@@ -27,16 +27,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('port', process.env.PORT || 5000);
-console.log("++++++++++++++++" + app.get('port'));
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, '/build')));
-app.use(express.static('./build'));
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(cors());
 
 app.use('/', productsRouter);
@@ -47,7 +44,7 @@ app.use('/checkout', checkoutRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('/build'));
+  app.use(express.static('./build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
