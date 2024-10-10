8e6257f8c09fa0dd5f7f0ec1633816efe44d01c6
Stas <stan.shm@gmail.com>
2024-10-09 20:36:09 -0500

    atempt to deploy to azure 3

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use(cors());
 
 app.use('/', productsRouter);
@@ -44,9 +44,9 @@ app.use('/checkout', checkoutRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('./build'));
+  app.use(express.static('./client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   });
 }
 
