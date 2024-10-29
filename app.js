c470d9777c02d86ddbf52ba60d86792d04a4e437
Stas <stan.shm@gmail.com>
2024-10-28 21:08:30 -0500

    added an allowed origin in CORS

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './client/build')));
 //app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: ['https://mernshop.azurewebsites.net','http://localhost:3000'],  
   methods: ['GET', 'POST', 'PUT', 'DELETE']  
 }));
 
