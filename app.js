1ac91c3a965552c060a584d2504355e129a2f6a8
Stas <stan.shm@gmail.com>
2024-10-29 21:00:28 -0500

    added path.join() to static path

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ app.use('/checkout', checkoutRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('./build'));
+  //app.use(express.static('build'));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'build','index.html'));
   });
