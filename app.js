aeea3634ba5e678809ddcd2c4109c88d19077f0a
Stas <stan.shm@gmail.com>
2024-10-29 21:24:26 -0500

    changes in app.js

--- app.js
+++ app.js
@@ -50,14 +50,14 @@ app.use('/products', productsRouter);
 app.use('/cart', cartRouter);
 app.use('/checkout', checkoutRouter);
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   //app.use(express.static('build'));
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'build','index.html'));
   });
-// }
+}
 
 
 // catch 404 and forward to error handler
