c67791f73b5fbabbeccc883000c07543fb70aeb7
Stas <stan.shm@gmail.com>
2024-10-29 20:07:03 -0500

    static files path change in app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './build')));
 //app.use(cors());
 app.use(cors({
   origin: ['https://mernshop.azurewebsites.net','http://localhost:3000'],  
