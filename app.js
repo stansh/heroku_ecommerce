9d02713474289a43bc3d2bd5ef8be339feec9789
Stas <stan.shm@gmail.com>
2024-11-03 16:42:41 -0600

    app.js routes change

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE']  
 }));
 
-app.use('/', indexRouter);
+app.use('/', productsRouter);
 //app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/cart', cartRouter);
