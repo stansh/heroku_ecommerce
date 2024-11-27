59bc241289a32d9a0a33fe3513140331756fb6ea
Stas <stan.shm@gmail.com>
2024-11-26 22:56:52 -0600

    Node upgrade

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(process.env.MONGODB_URL, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true
 });
 
 
